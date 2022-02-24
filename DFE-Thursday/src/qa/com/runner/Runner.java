be72fdf43ec4758635400040957a069324c553b9
LeonRobi <98519960+LeonRobi@users.noreply.github.com>
2022-02-24 16:19:11 +0000

    Add files via upload

---
+++ DFE-Thursday/src/qa/com/runner/Runner.java
@@ -0,0 +1,22 @@
+package qa.com.runner;
+
+import qa.com.iterations.Array;
+import qa.com.iterations.IterationsWhile;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		
+		// IterationsWhile.whileDemo();
+		// IterationsWhile.countdown();
+		// IterationsWhile.forLoop();
+		// IterationsWhile.flowLoop();
+		// IterationsWhile.counting(5);
+		// Array.arrayExample();
+		// Array.pizzaShop();
+		// Array.coffeShop();
+		Array.addItem();
+		Array.printarray();
+	}
+	
+}
